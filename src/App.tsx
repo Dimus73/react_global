19c556df6cc3e2e327544a63bddc219de4daafe0
Dmitry Prigozhin <v-dprigozhin@microsoft.com>
2024-05-18 12:46:11 +0300

    Class made function

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/className";
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {hovered: true, selected: false},[theme, 'cls2', 'cls3'])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
