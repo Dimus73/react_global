cecd47206dc1c855e5415dc04f78f94c0eff9a38
dmitryprigozhin <dmitry.prigozhin@gmail.com>
2024-09-19 09:52:17 +0300

    20 Тестовая среда. Настраиваем Jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,45 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass', {}, [])).toBe('someClass');
+    });
+
+    test('with additional param', () => {
+        const expected = 'someClass additionalParam';
+        expect(classNames('someClass', {}, ['additionalParam'])).toBe(expected);
+    });
+
+    test('with mode param', () => {
+        const expected = 'someClass additionalParam class1 class2';
+        expect(
+            classNames(
+                'someClass',
+                { class1: true, class2: true },
+                ['additionalParam'],
+            ),
+        ).toBe(expected);
+    });
+
+    test('with mode param, one is false', () => {
+        const expected = 'someClass additionalParam class1';
+        expect(
+            classNames(
+                'someClass',
+                { class1: true, class2: false },
+                ['additionalParam'],
+            ),
+        ).toBe(expected);
+    });
+
+    test('with mode param, one is undefined', () => {
+        const expected = 'someClass additionalParam class1';
+        expect(
+            classNames(
+                'someClass',
+                { class1: true, class2: undefined },
+                ['additionalParam'],
+            ),
+        ).toBe(expected);
+    });
+});
