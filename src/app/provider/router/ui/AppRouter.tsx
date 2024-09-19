247dce787a86fe448b705e9674e2a89b1d383c60
dmitryprigozhin <dmitry.prigozhin@gmail.com>
2024-09-19 10:35:18 +0300

    21 Несуществующие маршруты.

--- src/app/provider/router/ui/AppRouter.tsx
+++ src/app/provider/router/ui/AppRouter.tsx
@@ -1,27 +1,26 @@
-import React, {Suspense} from 'react';
-import {Route, Routes} from "react-router-dom";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
-import {routeConfig} from "shared/config/routeConfig/routerConfig";
+import React, { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { routeConfig } from 'shared/config/routeConfig/routerConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
-const AppRouter = () => {
-    return (
-        <Suspense fallback={<div>Loading</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({element, path}) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={(
-                            <div className="page-wrapper">
-                                {element}
-                            </div>
-                        )}
-                    />
-                    ))}
-            </Routes>
-        </Suspense>
-    );
-};
+const AppRouter = () => (
+    <Suspense fallback={<PageLoader />}>
+        <Routes>
+            {Object.values(routeConfig).map(({ element, path }) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={(
+                        <div className="page-wrapper">
+                            {element}
+                        </div>
+                    )}
+                />
+            ))}
+        </Routes>
+    </Suspense>
+);
 
 export default AppRouter;
